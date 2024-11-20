96225f247e12c399b71a0c4600caad0d1b81d240
Diana Shpakova <468118@niuitmo.ru>
2024-11-20 23:09:12 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -2,15 +2,15 @@ import circle
 import square
 import triangle
 
-figs = ['circle', 'square', 'triangle']
-funcs = ['perimeter', 'area']
+figs = ["circle", "square", "triangle"]
+funcs = ["perimeter", "area"]
 sizes = {
-    'circle-area': 1,
-    'circle-perimeter': 1,
-    'square-area': 1,
-    'square-perimeter': 1,
-    'triangle-area': 3,
-    'triangle-perimeter': 3
+    "circle-area": 1,
+    "circle-perimeter": 1,
+    "square-area": 1,
+    "square-perimeter": 1,
+    "triangle-area": 3,
+    "triangle-perimeter": 3,
 }
 
 
@@ -18,7 +18,7 @@ def calc(fig, func, size):
     assert fig in figs, "Invalid figure"
     assert func in funcs, "Invalid function"
 
-    key = f'{fig}-{func}'
+    key = f"{fig}-{func}"
     args = sizes.get(key)
     assert args is not None
     assert len(size) == args
@@ -29,13 +29,13 @@ def calc(fig, func, size):
         a, b, c = size
         assert a + b > c and a + c > b and b + c > a
 
-    result = eval(f'{fig}.{func}(*{size})')
+    result = eval(f"{fig}.{func}(*{size})")
     return result
 
 
 if __name__ == "__main__":
-    func = ''
-    fig = ''
+    func = ""
+    fig = ""
     size = []
 
     while fig not in figs:
@@ -45,9 +45,14 @@ if __name__ == "__main__":
         func = input(f"Enter function name, available are {funcs}:\n")
 
     while len(size) != sizes.get(f"{fig}-{func}", 1):
-        size = list(map(int, input(
-            "Input figure sizes separated by space, 1 for circle and square:\n"
-        ).split()))
+        size = list(
+            map(
+                int,
+                input(
+                    "Input figure sizes separated by space, 1 for circle and square:\n"
+                ).split(),
+            )
+        )
 
     result = calc(fig, func, size)
     print(f"Result: {result}")

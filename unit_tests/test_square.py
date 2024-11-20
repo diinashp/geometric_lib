6824f13491561afc54f941869627b7a40be0b5f6
Diana Shpakova <468118@niuitmo.ru>
2024-11-20 22:58:04 +0300

    Update test_square.py

--- unit_tests/test_square.py
+++ unit_tests/test_square.py
@@ -1,38 +1,36 @@
 import unittest
-from square import area, perimeter
+from square import area, perimeter  # Assuming your square module is named square.py
 
 
 class TestSquare(unittest.TestCase):
-    def test_area(self):
-        side = 1
-        res = area(side)
-        self.assertEqual(res, 1)
+    def test_area_positive(self):
+        side = 5
+        self.assertEqual(area(side), 25)
 
-    def test_perimeter(self):
-        side = 1
-        res = perimeter(side)
-        self.assertEqual(res, 4)
+    def test_perimeter_positive(self):
+        side = 5
+        self.assertEqual(perimeter(side), 20)
 
     def test_area_zero(self):
         side = 0
-        res = area(side)
-        self.assertEqual(res, 0)
+        self.assertEqual(area(side), 0)
 
     def test_perimeter_zero(self):
         side = 0
-        res = perimeter(side)
-        self.assertEqual(res, 0)
+        self.assertEqual(perimeter(side), 0)
 
     def test_area_negative(self):
         side = -1
-        if side < 0:
-            self.assertRaises(AssertionError)
+        with self.assertRaises(AssertionError) as context:
+            area(side)
+        self.assertEqual(str(context.exception), "Side cannot be negative")  
 
     def test_perimeter_negative(self):
         side = -1
-        if side < 0:
-            self.assertRaises(AssertionError)
+        with self.assertRaises(AssertionError) as context:
+            perimeter(side)
+        self.assertEqual(str(context.exception), "Side cannot be negative")
 
 
-if name == '__main__':
+if __name__ == '__main__':  
     unittest.main()

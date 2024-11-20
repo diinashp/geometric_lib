9ed72afda20e03a41b475a9a0ced202f95356348
Diana Shpakova <468118@niuitmo.ru>
2024-11-20 22:24:10 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,13 @@ import math
 
 
 def area(r):
+    if(r < 0):
+        raise AssertionError("error")
     return math.pi * r * r
 
 
 def perimeter(r):
+    if(r < 0):
+        raise AssertionError("error")
     return 2 * math.pi * r
 

8c80feb8905a3f41f6c5c33f44f50a163b1d512b
Diana Shpakova <468118@niuitmo.ru>
2024-11-20 22:28:39 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,11 @@
 
 def area(a):
+    if a < 0:
+        raise AssertionError("error")
     return a * a
 
 
 def perimeter(a):
+    if a < 0:
+        raise AssertionError("error")
     return 4 * a

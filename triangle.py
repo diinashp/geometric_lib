abfbe486a47a751a4493f5bf06036a1c94c298c4
Diana Shpakova <468118@niuitmo.ru>
2024-11-21 15:46:11 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,11 @@
 def area(a, b, c):
     p = (a + b + c) / 2
+    if a < 0 or b < 0 or c < 0:
+        raise AssertionError("error")
     return (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
 
 def perimeter(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        raise AssertionError("error")
     return a + b + c

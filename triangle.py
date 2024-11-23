3aba4a3abd8879f877f9e9606e5763365f79d673
Diana Shpakova <468118@niuitmo.ru>
2024-11-23 19:04:45 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,18 +1,19 @@
-def area(a, b, c): 
-    if a < 0 or b < 0 or c < 0:        
+def area(a, b, c):
+    if a < 0 or b < 0 or c < 0:
         raise AssertionError("error")
     if not (a + b > c and a + c > b and b + c > a):
-        if a == 0 and b == 0 and c == 0:            
+        if a == 0 and b == 0 and c == 0:
             return 0
-        raise AssertionError("error")    
-    p = (a + b + c) / 2    
+        raise AssertionError("error")
+    p = (a + b + c) / 2
     return (p * (p - a) * (p - b) * (p - c)) ** 0.5
-    
+
+
 def perimeter(a, b, c):
-    if a < 0 or b < 0 or c < 0:   
-        raise AssertionError("error")    
+    if a < 0 or b < 0 or c < 0:
+        raise AssertionError("error")
     if not (a + b > c and a + c > b and b + c > a):
         if a == 0 and b == 0 and c == 0:
-            return 0        
+            return 0
         raise AssertionError("error")
     return a + b + c
